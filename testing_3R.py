e61c0dffcd7282960e1341a8cae8007208f91c23
Tamizhanban212 <tamizhanbanag@gmail.com>
2026-02-10 13:00:45 +0530

    added complete danger calculating for one point

--- testing_3R.py
+++ testing_3R.py
@@ -6,18 +6,6 @@ from manipulator import ManipulatorApp   # assuming file is named manipulator.py
 root = tk.Tk()
 app = ManipulatorApp(root)
 
-# df_solver = DangerFields(k1=2.0, k2=0.5, gamma=1.1)
-# df, grad, control_vec = df_solver.compute_robot_danger(
-#     r_target=[1.0, 1.0, 0.0],
-#     r_starts=[[0, 0, 0], [1, 0, 0], [1.5, 0.5, 0]],
-#     r_ends=[[1, 0, 0], [1.5, 0.5, 0], [2.0, 1.0, 0]],
-#     v_starts=[[0, 0, 0], [0.1, 0, 0], [0.1, 0.1, 0]],
-#     v_ends=[[0.1, 0, 0], [0.1, 0.1, 0], [0.1, 0.1, 0]]
-# )
-
-# Set frame rate (example: 60 FPS)
-app.manipulator.set_frame_rate(60)
-
 # Test movements (uncomment one by one or run all)
 app.start_angle(a1=30, a2=30, a3=30, duration=2)
 
